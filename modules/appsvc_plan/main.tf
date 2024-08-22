20e9363e0a74108d0d6f23e5daf848fd53bab2c1
João Ribeiro <joaopedrosesimbra@gmail.com>
2024-08-22 18:29:42 +0100

    reverted tier, changed name

--- modules/appsvc_plan/main.tf
+++ modules/appsvc_plan/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_service_plan" "tfplan" {
-  name                = "tfplan"
+  name                = "tfplan1234"
   resource_group_name = "myTFResourceGroup"
   location            = "westeurope"
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
 }
